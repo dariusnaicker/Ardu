73cf1637c5ed5b83472eab6c981f1390af9f27a6
dariusnaicker <dariusnaicker28@gmail.com>
2025-08-10 19:02:38 +0200

    Add blink sketch for Mega

---
+++ Temp.ino
@@ -0,0 +1,11 @@
+// Blink the built-in LED on an Arduino Mega
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);  // Initialize pin as output
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn LED on
+  delay(1000);                      // Wait 1 second
+  digitalWrite(LED_BUILTIN, LOW);   // Turn LED off
+  delay(1000);                      // Wait 1 second
+}
